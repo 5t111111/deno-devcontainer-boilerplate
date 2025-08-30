26dc830048317f4c85f0e34a6db32bff0329961d
Hirofumi Wakasugi <333180+5t111111@users.noreply.github.com>
2025-08-30 19:27:25 +0900

    Add missing semicolon for consistency

--- main.ts
+++ main.ts
@@ -4,6 +4,6 @@ export function add(a: number, b: number): number {
 
 // Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
 if (import.meta.main) {
-  console.log(`DENO_ENV: ${Deno.env.get("DENO_ENV")}`)
+  console.log(`DENO_ENV: ${Deno.env.get("DENO_ENV")}`);
   console.log("Add 2 + 3 =", add(2, 3));
 }
